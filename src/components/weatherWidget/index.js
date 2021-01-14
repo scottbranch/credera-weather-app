cd0fa32e9924584475fba5d5f2fb70a5ff8df44b
Scott Branch <scottebranch@gmail.com>
2021-01-14 11:54:11 -0800

    setting up axios stuff

--- src/components/weatherWidget/index.js
+++ src/components/weatherWidget/index.js
@@ -1,6 +1,20 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 
 const WeatherWidget = props => {
+
+  const [city, setCity] = useState('dallas')
+  const [weatherResponse, setWeatherResponse] = useState(null)
+
+  useEffect(() => {
+    axios.get(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${process.env.WEATHER_API_KEY}`)
+    .then(response => console.log(response))
+    .catch(function (error) {
+      // handle error
+      console.log(error)
+    })
+  },[])
+
   return (
     <div>weather!</div>
   )
