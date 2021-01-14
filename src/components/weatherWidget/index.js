70a7b4dfb154a78b6f447a936bde34cb379f8447
Scott Branch <scottebranch@gmail.com>
2021-01-13 21:24:49 -0800

    setting up boilerplate code

---
+++ src/components/weatherWidget/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const WeatherWidget = props => {
+  return (
+    <div>weather!</div>
+  )
+}
+
+export default WeatherWidget
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,9 +5,12 @@ import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
+import WeatherWidget from '../components/weatherWidget'
+
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
+    <WeatherWidget/>
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
