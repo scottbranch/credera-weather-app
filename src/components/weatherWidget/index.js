ae3b3bb840c36cfc0a86350e628bbf3be7d9b24f
Scott Branch <scottebranch@gmail.com>
2021-01-14 19:55:28 -0800

    setting up api to get data and sync icons

--- src/components/weatherWidget/index.js
+++ src/components/weatherWidget/index.js
@@ -1,22 +1,46 @@
-import React, { useEffect, useState } from 'react'
-import axios from 'axios'
+import React, { useEffect, useState } from "react"
+import axios from "axios"
+
+import WeatherBlock from "./weatherBlock"
+
+import { weatherIcons } from "./iconMap"
 
 const WeatherWidget = props => {
-
-  const [city, setCity] = useState('dallas')
-  const [weatherResponse, setWeatherResponse] = useState(null)
+  const [city, setCity] = useState("dallas")
+  const [unit, setUnit] = useState("imperial")
+  const [icons, setIcons] = useState([])
+  const [weatherResponse, setWeatherResponse] = useState([])
 
   useEffect(() => {
-    axios.get(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${process.env.GATSBY_WEATHER_API_KEY}`)
-    .then(response => console.log(response))
-    .catch(function (error) {
-      // handle error
-      console.log(error)
-    })
-  },[])
+    axios
+      .get(
+        `https://api.openweathermap.org/data/2.5/onecall?lat=32.7668&lon=-96.7836&units=${unit}&exclude=minutely,hourly&appid=${process.env.GATSBY_WEATHER_API_KEY}`
+      )
+      .then(response => {
+        setWeatherResponse(response.data)
+        console.log(response.data)
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error)
+      })
+  }, [])
 
   return (
-    <div>weather!</div>
+    <>
+      <div>weather!</div>
+      {weatherResponse.daily?.map((item, index) => {
+        let iconId = item.weather[0].id
+
+        return (
+          <WeatherBlock
+            iconClass={`wi wi-${weatherIcons[iconId].icon}`}
+            key={index}
+            title={item.weather[0].description}
+          />
+        )
+      })}
+    </>
   )
 }
 
