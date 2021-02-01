6ee368e7340282d0bd513f36f8ea2daf82c78aa1
Scott Branch <scottebranch@gmail.com>
2021-02-01 11:55:55 -0800

    spinner clone

--- src/components/weatherWidget/spinner.js
+++ src/components/weatherWidget/spinner.js
@@ -4,7 +4,7 @@ import styled, { keyframes } from "styled-components"
 const Spinner = props => {
   const { size } = props
 
-  return <LoaderContainer size={size}>this is the og</LoaderContainer>
+  return <LoaderContainer size={size}>this is the og adding clone line</LoaderContainer>
 }
 
 const spinner = keyframes`
